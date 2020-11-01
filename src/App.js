498858c2b1f25c50ffc7092fc77048aa35119a03
ricardocvel <ricardocampos_pr@live.com>
2020-11-01 15:11:45 -0300

    separando  listas de App

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './style/Global.css';
 import './style/App.css';
 
 import Header from './Header';
-//import Itens from './components/Item';
-import Carne from './assets/cardapio/CArne.png'
+import Itens from './components/Item';
+
 
 function App() {
   return (
@@ -14,19 +14,7 @@ function App() {
       <div>
         <div className="cardapio">
           <ul>
-              <li className="cardItem">
-                <img src={Carne}/>
-                  <h1>teste</h1>
-              </li>
-              <li className="cardItem">
-                  <h1>teste</h1>
-              </li>
-              <li className="cardItem">
-                  <h1>teste</h1>
-              </li>
-              <li className="cardItem">
-                  <h1>teste</h1>
-              </li>
+              <Itens/>
           </ul>
         </div>
       </div>
