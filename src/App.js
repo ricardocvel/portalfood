5396cc34dd0b64cd2e6c9e27582195c649513b6a
ricardocvel <ricardocampos_pr@live.com>
2020-11-01 13:24:21 -0300

    Teste de listas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
 
-import Header from './Header';
+import './style/Global.css';
+import './style/App.css';
 
+import Header from './Header';
+//import Itens from './components/Item';
+import Carne from './assets/cardapio/CArne.png'
 
 function App() {
   return (
     <> 
       <Header/>
-        <h1>hello world </h1>
-
+      <div>
+        <div className="cardapio">
+          <ul>
+              <li className="cardItem">
+                <img src={Carne}/>
+                  <h1>teste</h1>
+              </li>
+              <li className="cardItem">
+                  <h1>teste</h1>
+              </li>
+              <li className="cardItem">
+                  <h1>teste</h1>
+              </li>
+              <li className="cardItem">
+                  <h1>teste</h1>
+              </li>
+          </ul>
+        </div>
+      </div>
     </>
 
   );
