e01ee367970e841d2ac4673298cfa28db2e2970a
ricardocvel <ricardocampos_pr@live.com>
2020-11-01 19:11:19 -0300

    Fim header

--- src/Header.js
+++ src/Header.js
@@ -3,19 +3,34 @@ import React from 'react';
 import './style/Header.css';
 
 import Lupa from './assets/icons/search.svg';
-import Logo from './assets/logoColor.png';
+import Instagram from './assets/svg/instagram.svg'; 
+import Facebook from './assets/svg/facebook.svg';
+import Location from './assets/svg/location-pin.svg';
+
+
+import Logo from './assets/logoCinza.png';
+import LogoR from './assets/logolanche.png';
 
 export default function Header() {
     return(
         <div id="header">
-            <div> 
-                
-                <h1>Shawarmaria</h1>
+            <div className="containerleft"> 
+                <img src={LogoR}/>
             </div>
             <div className="containerright"> 
                 <div className="pesquisar">
+                    <input placeholder='Pesquisar' ></input>
                     <img src={Lupa}/>
-                    <input></input>
+
+                    <img src={Instagram}/>
+                    <img src={Facebook}/>
+                </div>
+                <div className="titulo">
+                    <div>
+                        <h1>BEM VINDO A SHAWARMARIA</h1>
+                        <h2>Escolha o item de sua preferencia e clique no botão </h2>
+                        <h2>Pedir pelo Whatsapp</h2>
+                    </div>
                 </div>
             </div>
         </div>
